f06712aea115b0ec3aabbc12a9bd7dcda945abb9
Julien Delsescaux <jdelsescaux@skillbase.fr>
2019-03-14 18:55:10 +0100

    synchro

--- autoload.php
+++ autoload.php
@@ -19,15 +19,16 @@ if(is_file(PHI_LIB_PATH.'/phi-core/get-autoloader-with-phi.php')) {
 
     $autoloader->addNamespace('Planck', $planckPath.'/planck/source/class');
     $autoloader->addNamespace('Planck\Application', $planckPath.'/planck-application/source/class');
-
-    $autoloader->addNamespace('Planck\ApplicationBuilder', $planckPath.'/planck-application-builder/source/class');
-
-
-    $autoloader->addNamespace('Planck\Routing', $planckPath.'/planck-routing/source/class');
-    $autoloader->addNamespace('Planck\Pattern', $planckPath.'/planck-pattern/source/class');
     $autoloader->addNamespace('Planck\Model', $planckPath.'/planck-model/source/class');
-    $autoloader->addNamespace('Planck\View', $planckPath.'/planck-view/source/class');
-    $autoloader->addNamespace('Planck\Navigation', $planckPath.'/planck-navigation/source/class');
+    $autoloader->addNamespace('Planck\Routing', $planckPath.'/planck-routing/source/class');
+
+
+    //$autoloader->addNamespace('Planck\ApplicationBuilder', $planckPath.'/planck-application-builder/source/class');
+    //$autoloader->addNamespace('Planck\Routing', $planckPath.'/planck-routing/source/class');
+    //$autoloader->addNamespace('Planck\Pattern', $planckPath.'/planck-pattern/source/class');
+    //$autoloader->addNamespace('Planck\Model', $planckPath.'/planck-model/source/class');
+    //$autoloader->addNamespace('Planck\View', $planckPath.'/planck-view/source/class');
+    //$autoloader->addNamespace('Planck\Navigation', $planckPath.'/planck-navigation/source/class');
 
     return $autoloader;
 
